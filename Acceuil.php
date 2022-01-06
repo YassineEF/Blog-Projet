d01e55e016856c6e985fb2de5d23081399e32bc5
YassineEF <yassineonline@outlook.fr>
2022-01-06 17:10:17 +0100

    :tada:Begin coding(Begin Header)

---
+++ Acceuil.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="9CBBA8">
+    <title>YassoShoes</title>
+    <link rel="stylesheet"  href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <section id="topLeft">
+            <img src="./img/LogoWhite.png" alt="Logo White">
+        </section>
+        <section id="topCenter">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shoes</a></li>
+                <li><a href="#">Brand</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </section>
+        <section id="topRight">
+            <a href="#">Login</a>
+        </section>
+    </header>
+</body>
+</html>
